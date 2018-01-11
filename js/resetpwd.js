7a989358cfe95ea03e896b7d828332f6c908580f
wangxing <fozei@qq.com>
2018-01-11 22:29:03 +0800

    修改密码流程更改

--- js/resetpwd.js
+++ js/resetpwd.js
@@ -1,16 +1,16 @@
 function finish() {
-    var phone = $("#phone").val();
-    if (!(/^1[3|4|5|7|8][0-9]\d{4,8}$/.test($.trim(phone)))) {
-        layer.open({
-            content: "请输入正确的账号或密码"
-            , skin: 'msg'
-            , time: 2 //2秒后自动关闭
-        });
-        return false;
-    }
+    // var phone = $("#pwd1").val();
+    // if (!(/^1[3|4|5|7|8][0-9]\d{4,8}$/.test($.trim(phone)))) {
+    //     layer.open({
+    //         content: "请输入正确的账号或密码"
+    //         , skin: 'msg'
+    //         , time: 2 //2秒后自动关闭
+    //     });
+    //     return false;
+    // }
 
-    var pwd = $.trim($('#pwd').val());
-    if (pwd.length < 6) {
+    var pwd1 = $.trim($('#pwd1').val());
+    if (pwd1.length < 6) {
         layer.open({
             content: "请输入6-18位密码"
             , skin: 'msg'
@@ -19,16 +19,30 @@ function finish() {
         return;
     }
 
-    var verify = getQueryString("var");
-    var phone1 = decodePwd(verify);
-
-    console.log(phone + ":::" + phone1);
-
-    if (phone1 !== phone) {
+    var pwd2 = $.trim($('#pwd2').val());
+    if (pwd2.length < 6) {
+        layer.open({
+            content: "请输入6-18位密码"
+            , skin: 'msg'
+            , time: 2 //2秒后自动关闭
+        });
         return;
     }
 
-    var realpwd = encryptPwd(pwd);
+    if (pwd1 !== pwd2) {
+        layer.open({
+            content: "两次输入的密码不一致"
+            , skin: 'msg'
+            , time: 2 //2秒后自动关闭
+        });
+        return;
+    }
+
+    var verify = getQueryString("var");
+    var phone = decodePwd(verify);
+
+
+    var realpwd = encryptPwd(pwd1);
 
     $('#finish').attr("onclick", null);
     $('#finish').css("background-color", "grey");
