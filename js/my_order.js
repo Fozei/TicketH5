cef03e473e7dbb49ec33a75cdccc09bf041151e8
wangxing <fozei@qq.com>
2018-01-15 19:59:16 +0800

    我的订单页面UI重构，弹层展示订单下所有票信息

--- js/my_order.js
+++ js/my_order.js
@@ -1,3 +1,38 @@
+var pageData;
+
+function buildUpOrderContent(index) {
+    var dataItem = pageData[parseInt(index)];
+    var str;
+    if (dataItem.status === "1") {
+        str = "已支付：";
+    } else {
+        str = "未支付：";
+    }
+
+    var week = dataItem.week + "\t";
+
+    console.log(dataItem);
+
+    var content = "<div class=\"page\" onclick=\"showTicketList(" + index + ")\">\n" +
+        "        <span class=\"sp-ft-title\">" + dataItem.matchName + "<span class=\"sp-ft-details\">比赛赛事：</span><span>" + dataItem.team + "</span></span>\n" +
+        "        <img class=\"img-line\" src=\"images/line.png\">\n" +
+        "        <div style=\"float: left;\">\n" +
+        "            <img class=\"img-pictrue\" src=\"" + dataItem.matchPic + "\">\n" +
+        "        </div>\n" +
+        "        <div class=\"d-details\">\n" +
+        "            <p>购票时间：" + dataItem.create_time + "</p>\n" +
+        "            <p>比赛时间：" + dataItem.year_r + "</p>\n" +
+        "            <p style=\"margin-left: 2.3555rem\">周" + week + dataItem.match_time + "</p>\n" +
+        "            <p>数&#12288&#12288量：" + dataItem.seatList.length + "</p>\n" +
+        "            <p>支付状态：" + str + "￥" + dataItem.total_price + " </p>\n" +
+        "        </div>\n" +
+        "        <div class=\"clear\"></div>\n" +
+        "    </div>";
+
+
+    return content;
+}
+
 $(function () {
 
     $.ajax({
@@ -11,8 +46,9 @@ $(function () {
         dataType: "json",
         success: function (data) {
             // layer.close(index);
-            if (data.code == 'success') {
-                if (data.list == 0) {
+            if (data.code === 'success') {
+                console.log(data);
+                if (data.list === 0) {
                     $('#dev_ul').append('<div class="emptyTip">暂无信息</div>');
                 } else {
                     create_details_data(data.list);
@@ -24,64 +60,10 @@ $(function () {
     });
 
     function create_details_data(data) {
-        console.log(data);
         var htmls = '';
-        var str = '';
         $.each(data, function (v) {
-            if (data[v].status == "1") {
-                str = "已支付：";
-            } else {
-                str = "未支付：";
-            }
-            var p_html = getdata(data[v].seatList);
-
-            // htmls += "<div class=\"page\">\n" +
-            //     "        <span class=\"sp-ft-title\">" + data[v].matchName + "<span class=\"sp-ft-details\">比赛赛事：</span><span class=\"\">" + data[v].team + "</span></span>\n" +
-            //     "        <img class=\"img-line\" src=\"images/line.png\">\n" +
-            //     "        <div style=\"float: left;\">\n" +
-            //     "            <img class=\"img-pictrue\" src=\'" + data[v].matchPic + "\'>\n" +
-            //     "        </div>\n" +
-            //     "        <div class=\"d-details\">\n" + p_html +
-            //     "            <p style=\"font-size: 0.474rem; color: #808080;\">比赛时间：<span style=\"font-size: 0.474rem; color: #1a1a1a;\">" + data[v].year_r + "</span>\n" +
-            //     "            </p>\n" +
-            //     "            <p style=\"font-size: 0.474rem; color: #1a1a1a; margin-left: 2.3555rem\">" + "周" + data[v].week + "&nbsp&nbsp&nbsp&nbsp" + data[v].match_time + "</p>\n" +
-            //     "        </div>\n" +
-            //     "        <span style=\"float: left;position: absolute;right: 1.259rem;top: 5rem;color: #545454;font-size: 0.474rem\">共计<span>" + data[v].buy_num + "</span>张</span>\n" +
-            //     "        <div class=\"clear\"></div>\n" +
-            //     "        <div style=\"position: relative; margin-top: 0.237rem;height: 2rem\">\n" +
-            //     "            <p style=\"position: absolute; color: #545454; font-size: 0.474rem;left: 1.185rem;height: 1rem\">\n" +
-            //     "                购票时间：" + data[v].create_time + "</p>\n" +
-            //     "            <P style=\"position: absolute; right: 1.259rem;color: #545454;font-size: 0.474rem;height: 1rem\">" + str + "<span\n" +
-            //     "                    style=\"color: #ec1c63;\">￥" + data[v].total_price + "</span></P>\n" +
-            //     "            <p style=\"position: absolute; color: #545454; font-size: 0.474rem;left: 1.185rem;top: 1rem\">\n" +
-            //     "                身份证号：130133198812082375</p>\n" +
-            //     "            <P style=\"position: absolute; right: 1.259rem;color: #545454;font-size: 0.474rem;height: 1rem;top: 1rem\">\n" +
-            //     "                姓&#8194&#8194名：<span\n" +
-            //     "                    style=\"color: #545454;\">张三</span></P>\n" +
-            //     "        </div>\n" +
-            //     "    </div>";
-
-
-            htmls += "<div class='page'> " +
-                "<span class='sp-ft-title'>" + data[v].matchName + "<span class='sp-ft-details'>&nbsp&nbsp比赛赛事：</span><span class=''>" + data[v].team + "</span></span>" +
-                " <img class='img-line' src='images/line.png'> " +
-                " <div style='float: left;'>" +
-                "<img class='img-pictrue' src='" + data[v].matchPic + "'> " +
-                " </div> " +
-                "<div class='d-details'> " + p_html +
-                // "<p style='font-size: 0.474rem; color: #808080;letter-spacing: 0.15rem;'>座位号：<span style='font-size: 0.474rem; color: #1a1a1a;letter-spacing: 0rem;'>A区2排39座</span></p>" +
-                "<p style='font-size: 0.474rem; color: #808080;'>比赛时间：<span style='font-size: 0.474rem; color: #1a1a1a;'>" + data[v].year_r + "</span></p> " +
-                "<p style='font-size: 0.474rem; color: #1a1a1a; margin-left: 2.3555rem'>周" + data[v].week + "&nbsp&nbsp" + data[v].match_time + "</p> " +
-                "</div>" +
-                "<span style='float: left;position: absolute;right: 1.259rem;top: 5rem;color: #545454;font-size: 0.474rem'>共计<span>" + data[v].buy_num + "</span>张</span>" +
-                "<div class='clear'></div>" +
-                "<div style='position: relative; margin-top: 0.237rem;'>" +
-                "<span style='position: absolute; color: #545454; font-size: 0.474rem;left: 1.185rem'>购票时间：" + data[v].create_time + "</span>" +
-                "<span style='position: absolute; right: 1.259rem;color: #545454;font-size: 0.474rem;'>" + str + "<span style='color: #ec1c63;'>￥" + data[v].total_price + "</span></span>" +
-                "<span style='position: absolute; color: #545454; font-size: 0.474rem;left: 1.185rem;top: 1rem'>证件号码：" + data[v].create_time + "</span>" +
-                "<span style='position: absolute; right: 1.259rem;color: #545454;font-size: 0.474rem;top: 1rem'>" + str + "<span>￥" + data[v].total_price + "</span></span>" +
-                "</div>" +
-                "</div>";
+            pageData = data;
+            htmls += buildUpOrderContent(v);
         });
         $('#dev_ul').append(htmls);
     }
@@ -95,3 +77,81 @@ $(function () {
     }
 
 })
+
+function buildUpContent(pageDatum) {
+    console.log(pageDatum);
+    var content = "<div class=\"orderDetail\"><p class=\"layer_title\">" + pageDatum.matchName + "</p><ul class=\"ticketUl\">";
+
+    var seatList = pageDatum.seatList;
+    for (var i = 0; i < seatList.length; i++) {
+        var seatInfo = seatList[i];
+        var seatArea = seatInfo.areaName + "区" + seatInfo.matchseat_line + "排" + seatInfo.area_id + "座";
+
+        var payStatus;
+        if (pageDatum.status === "1") {
+            payStatus = "已支付";
+        } else {
+            payStatus = "未支付";
+        }
+
+        var matchDate = pageDatum.year_r + "\t" + pageDatum.match_time;
+
+        var str = "<li style=\"margin-top: 0.5rem\">\n" +
+            "        <div>\n" +
+            "        <p class=\"left\">比赛时间：<span>" + matchDate + "</span></p>\n" +
+            "    <p class=\"right\">座&#8194位&#8194号：<span>" + seatArea + "</span></p>\n" +
+            "    </div>\n" +
+            "    <div>\n" +
+            "    <p class=\"left\">订&#8194单&#8194号：<span>" + pageDatum.num + "</span></p>\n" +
+            "    <p class=\"right\">支付状态：<span>" + payStatus + "</span></p>\n" +
+            "    </div>\n" +
+            "    <div>\n" +
+            "    <p class=\"left\">票&#12288&#12288价：<span>" + seatInfo.matchseat_price + "</span></p>\n" +
+            "    <p class=\"right\">姓&#12288&#12288名：<span>" + seatInfo.name + "</span></p>\n" +
+            "    </div>\n" +
+            "    <div>\n" +
+            "    <p class=\"left\">电话号码：<span>" + pageDatum.tel + "</span></p>\n" +
+            "    <p class=\"right\">身份证号：<span>" + seatInfo.idcard + "</span></p>\n" +
+            "    </div>\n" +
+            "    </li>";
+
+
+        content += str;
+    }
+
+
+    return content += "</ul></div>";
+}
+
+function showTicketList(index) {
+    if (pageData !== null && pageData !== undefined) {
+        //自定义标题风格
+        var content = buildUpContent(pageData[parseInt(index)]);
+
+        layer.open({
+            type: 1,
+            title: false,
+            skin: 'layui-layer-demo', //样式类名
+            closeBtn: 0, //不显示关闭按钮
+            anim: 2,
+            shade: [0.9, '#a8c4f2'],
+            shadeClose: true, //开启遮罩关闭
+            area: ['851px', '851px'],
+            content: content
+        });
+
+        // layer.open({
+        //     title: [
+        //         '订单详情',
+        //         'background-color: #2d70e1; color:#fff;'
+        //     ]
+        //     , content: content
+        // });
+    } else {
+        layer.open({
+            content: '糟糕，页面出错'
+            , skin: 'msg'
+            , time: 2 //2秒后自动关闭
+        });
+    }
+}
