38685fe01515293183748bad29a2c5815ec94200
web <13395308359@163.com>
2018-03-14 15:02:56 +0800

    动态获取场地图片2

--- js/constance.js
+++ js/constance.js
@@ -1,5 +1,5 @@
 var COOKIE_NAME_USER_ID = "ewedouserID";
-//var DOMAIM_API='http://192.168.1.78/ticket/';
+// var DOMAIM_API='http://192.168.1.78/ticket/';
 var DOMAIM_API='http://www.ewedo.net/ticket/';
 var DOMAIN = DOMAIM_API+"api";
 var MALL_ADDRESS =
